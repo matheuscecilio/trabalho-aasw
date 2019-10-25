3cd6efe4402b6192aef2c02cf5688dc3e3688d06
Matheus Alberto <maatheusaalberto@gmail.com>
2019-10-24 23:23:03 -0300

    Implementação EndPoint 02 + melhoria de código

--- template-facebook/src/main/java/aas/exemplos/ContaController.java
+++ template-facebook/src/main/java/aas/exemplos/ContaController.java
@@ -4,6 +4,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @RestController
 public class ContaController {
@@ -27,6 +33,8 @@ public class ContaController {
 	private static final String CLIENT_ID = "422169628682963";
 	private static final String CLIENT_SECRET = "7ddb5d7f1e6b6938a772b28a768ea12e";
 	private static final String ACCESS_TOKEN = "422169628682963|1VV0U8_WmKUptyE0DQFybaUPDDU";
+	private static final String URL_BASE_REDIRECT = "http://localhost:8080/conta/";
+	private static final String URL_BASE_CADASTRO_REDIRECT = URL_BASE_REDIRECT + "cadastro/";
 	
 	private List<Conta> registros = new ArrayList<Conta>();
 	
@@ -46,7 +54,7 @@ public class ContaController {
 		String facebook_login_url = "https://www.facebook.com/v3.3/dialog/oauth?client_id=%s&redirect_uri=%s&state=%s"; 
 		int new_id = registros.size() + 1;
 		String state = "security_text";
-		String url_to_redirect ="http://localhost:8080/conta/cadastro/" + new_id; 
+		String url_to_redirect = URL_BASE_CADASTRO_REDIRECT + new_id; 
 		String facebook_login_url_formated = String.format(facebook_login_url, CLIENT_ID, url_to_redirect, state);
 				
 		HttpHeaders headers = new HttpHeaders();
@@ -56,20 +64,50 @@ public class ContaController {
 	}
 	
 	@GetMapping("/conta/cadastro/{id}")
-	public ResponseEntity<Conta> cadastrarConta(@PathVariable("id") Long id, @PathParam("code") String code, @PathParam("state") String state) {
+	public ResponseEntity<Conta> cadastrarConta(@PathVariable("id") Long id, @PathParam("code") String code, @PathParam("state") String state) throws URISyntaxException {
 		
-		throw new UnsupportedOperationException("Endpoint02 não implementado");
+		String url_busca_token_acesso = "https://graph.facebook.com/v3.3/oauth/access_token?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&state={STATE}&client_secret={SECRET}&code={CODE}";
+		url_busca_token_acesso = url_busca_token_acesso.replace("{CLIENT_ID}", CLIENT_ID);
+		url_busca_token_acesso = url_busca_token_acesso.replace("{REDIRECT_URI}", URL_BASE_CADASTRO_REDIRECT + id);
+		url_busca_token_acesso = url_busca_token_acesso.replace("{STATE}", state);
+		url_busca_token_acesso = url_busca_token_acesso.replace("{SECRET}", CLIENT_SECRET);
+		url_busca_token_acesso = url_busca_token_acesso.replace("{CODE}", code);
 		
-		// Endpoint 02:
-		// Realiza uma série de 3 chamadas para o servidor do facebook
-		//     1) Envia o code para obter o token de acesso;
-		//     2) Envia o token para ser auditado e obter id do usuário; 
-		//     3) Obtem os dados da conta.
-		// 
-		// Salva os dados do usuário recebidos na terceira requisição
-		//
-		// Retorna um redirecionamento para o usuário:
-		//     O status da resposta é um http redirect 307;
-		//     Adiciona na resposta um cabeçalho "Location", cujo valor é o Endpoint 01 com o identificador do usuário  
+		RestTemplate restTemplate = new RestTemplate();    
+	     
+		String resp = restTemplate.getForObject(url_busca_token_acesso, String.class);
+		
+	    JsonObject jsonObject = new JsonParser().parse(resp).getAsJsonObject();
+	    String input_token = jsonObject.get("access_token").getAsString();
+
+	    String url_busca_data_user_id = "https://graph.facebook.com/debug_token?input_token={INPUT_TOKEN}&access_token={ACCESS_TOKEN}";
+	    url_busca_data_user_id = url_busca_data_user_id.replace("{INPUT_TOKEN}", input_token);
+		url_busca_data_user_id = url_busca_data_user_id.replace("{ACCESS_TOKEN}", ACCESS_TOKEN);
+		
+		resp = restTemplate.getForObject(url_busca_data_user_id, String.class);
+		
+	    jsonObject = new JsonParser().parse(resp).getAsJsonObject();
+	    String user_id = jsonObject.get("data").getAsJsonObject().get("user_id").getAsString();
+	    
+	    String url_busca_usuario = "https://graph.facebook.com/{ID_USUARIO_FACEBOOK}/?access_token={INPUT_TOKEN}";
+	    url_busca_usuario = url_busca_usuario.replace("{ID_USUARIO_FACEBOOK}", user_id);
+	    url_busca_usuario = url_busca_usuario.replace("{INPUT_TOKEN}", input_token);
+	    
+	    resp = restTemplate.getForObject(url_busca_usuario, String.class);
+	    
+	    jsonObject = new JsonParser().parse(resp).getAsJsonObject();
+	    String user_facebook_id = jsonObject.get("id").getAsString();
+	    String user_facebook_name = jsonObject.get("name").getAsString();
+	    
+	    Conta c = new Conta();
+	    c.setId(id);
+	    c.setFacebookId(user_facebook_id);
+	    c.setNome(user_facebook_name);
+	    registros.add(c);
+
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Location", URL_BASE_REDIRECT + id);
+				
+		return new ResponseEntity<>(null, headers, HttpStatus.TEMPORARY_REDIRECT);
 	}
 }
