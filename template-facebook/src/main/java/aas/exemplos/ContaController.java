b9f30671e6d096d802f8fc9456031ee5c4c9d058
Matheus Alberto <maatheusaalberto@gmail.com>
2019-10-25 22:29:00 -0300

    Remoção de imports e validação para não cadastrar 2x um mesmo usuário

--- template-facebook/src/main/java/aas/exemplos/ContaController.java
+++ template-facebook/src/main/java/aas/exemplos/ContaController.java
@@ -1,23 +1,13 @@
 package aas.exemplos;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.websocket.server.PathParam;
 
-import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +24,6 @@ public class ContaController {
 	private static final String CLIENT_SECRET = "7ddb5d7f1e6b6938a772b28a768ea12e";
 	private static final String ACCESS_TOKEN = "422169628682963|1VV0U8_WmKUptyE0DQFybaUPDDU";
 	private static final String URL_BASE_REDIRECT = "http://localhost:8080/conta/";
-	private static final String URL_BASE_CADASTRO_REDIRECT = URL_BASE_REDIRECT + "cadastro/";
 	
 	private List<Conta> registros = new ArrayList<Conta>();
 	
@@ -54,7 +43,7 @@ public class ContaController {
 		String facebook_login_url = "https://www.facebook.com/v3.3/dialog/oauth?client_id=%s&redirect_uri=%s&state=%s"; 
 		int new_id = registros.size() + 1;
 		String state = "security_text";
-		String url_to_redirect = URL_BASE_CADASTRO_REDIRECT + new_id; 
+		String url_to_redirect = URL_BASE_REDIRECT + "cadastro/" + new_id; 
 		String facebook_login_url_formated = String.format(facebook_login_url, CLIENT_ID, url_to_redirect, state);
 				
 		HttpHeaders headers = new HttpHeaders();
@@ -68,7 +57,7 @@ public class ContaController {
 		
 		String url_busca_token_acesso = "https://graph.facebook.com/v3.3/oauth/access_token?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&state={STATE}&client_secret={SECRET}&code={CODE}";
 		url_busca_token_acesso = url_busca_token_acesso.replace("{CLIENT_ID}", CLIENT_ID);
-		url_busca_token_acesso = url_busca_token_acesso.replace("{REDIRECT_URI}", URL_BASE_CADASTRO_REDIRECT + id);
+		url_busca_token_acesso = url_busca_token_acesso.replace("{REDIRECT_URI}", URL_BASE_REDIRECT + "cadastro/" + id);
 		url_busca_token_acesso = url_busca_token_acesso.replace("{STATE}", state);
 		url_busca_token_acesso = url_busca_token_acesso.replace("{SECRET}", CLIENT_SECRET);
 		url_busca_token_acesso = url_busca_token_acesso.replace("{CODE}", code);
@@ -99,11 +88,21 @@ public class ContaController {
 	    String user_facebook_id = jsonObject.get("id").getAsString();
 	    String user_facebook_name = jsonObject.get("name").getAsString();
 	    
-	    Conta c = new Conta();
-	    c.setId(id);
-	    c.setFacebookId(user_facebook_id);
-	    c.setNome(user_facebook_name);
-	    registros.add(c);
+	    Conta conta = null;
+		for(Conta c : registros) {
+			if(c.getFacebookId().equals(user_facebook_id)) {
+				conta = c;
+				id = conta.getId();
+			}
+		}
+	    
+		if(conta == null) {
+			conta = new Conta();
+		    conta.setId(id);
+		    conta.setFacebookId(user_facebook_id);
+		    conta.setNome(user_facebook_name);
+		    registros.add(conta);
+		}
 
 	    HttpHeaders headers = new HttpHeaders();
 	    headers.add("Location", URL_BASE_REDIRECT + id);
